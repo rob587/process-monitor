5ae13534c84f7e6c1f12f2d50779c54c5f60eb17
rob587 <robertocamm02@yahoo.com>
2026-02-28 00:14:02 +0100

    Add initial implementation of process monitoring functionality

--- process-monitor.js
+++ process-monitor.js
@@ -1,3 +1,20 @@
+//importo i dati
+
 const si = require("systeminformation");
 const Table = require("cli-table3");
 const chalk = require("chalk");
+
+let prevData = new Map();
+
+//avvio una funzione in cui applico il filtraggio  e ri-ordino i livelli di Cpu
+
+async function getProcesses() {
+  const processes = await si.processes();
+
+  let topProcesses = processes.list
+    .filter((p) => p.cpu > 0 || p.mem > 0.5)
+    .sort((a, b) => b.cpu - a.cpu)
+    .slice(0, 15);
+
+  return topProcesses;
+}
